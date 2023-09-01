06bf05ba92d818ebd7b09a06c71337feb411a114
Jasmin Flores <jasminf2202@gmail.com>
2023-09-01 12:45:52 -0400

    repurposed functions and made cleaner flow

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,55 @@
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
+var counter = document.querySelector();
+var startButton = document.querySelector();
 
-// The startGame function is called when the start button is clicked
-function startGame() {
-    isWin = false;
-    timerCount = 10;
-    // Prevents start button from being clicked when round is in progress
-    startButton.disabled = true;
-    renderBlanks()
-    startTimer()
-  }
-  
-  // The winGame function is called when the win condition is met
-  function winGame() {
-    wordBlank.textContent = "YOU WON!!!🏆 ";
-    winCounter++
-    startButton.disabled = false;
-    setWins()
-  }
-  
-  // The loseGame function is called when timer reaches 0
-  function loseGame() {
-    wordBlank.textContent = "GAME OVER";
-    loseCounter++
-    startButton.disabled = false;
-    setLosses()
-  }
 
-  // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-function startTimer() {
-    // Sets timer
-    timer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        // Tests if win condition is met
-        if (isWin && timerCount > 0) {
-          // Clears interval and stops timer
-          clearInterval(timer);
-          winGame();
-        }
-      }
-      // Tests if time has run out
-      if (timerCount === 0) {
-        // Clears interval
-        clearInterval(timer);
-        loseGame();
-      }
-    }, 1000);
-  }
+// // Attach event listener to start button to call startGame function on click
+// startButton.addEventListener("click", startGame);
+
+// // The startGame function is called when the start button is clicked
+// function startGame() {
+//     isWin = false;
+//     timerCount = 10;
+//     // Prevents start button from being clicked when round is in progress
+//     startButton.disabled = true;
+//     renderBlanks()
+//     startTimer()
+//   }
+  
+//   // The winGame function is called when the win condition is met
+//   function winGame() {
+//     wordBlank.textContent = "YOU WON!!!🏆 ";
+//     winCounter++
+//     startButton.disabled = false;
+//     setWins()
+//   }
+  
+//   // The loseGame function is called when timer reaches 0
+//   function loseGame() {
+//     wordBlank.textContent = "GAME OVER";
+//     loseCounter++
+//     startButton.disabled = false;
+//     setLosses()
+//   }
+
+//   // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+// function startTimer() {
+//     // Sets timer
+//     timer = setInterval(function() {
+//       timerCount--;
+//       timerElement.textContent = timerCount;
+//       if (timerCount >= 0) {
+//         // Tests if win condition is met
+//         if (isWin && timerCount > 0) {
+//           // Clears interval and stops timer
+//           clearInterval(timer);
+//           winGame();
+//         }
+//       }
+//       // Tests if time has run out
+//       if (timerCount === 0) {
+//         // Clears interval
+//         clearInterval(timer);
+//         loseGame();
+//       }
+//     }, 1000);
+//   }
